c0c81097df04ef08b1b60edbf5956141c2cbad18
Steve Schwarzmann <schwa074@csusm.edu>
2025-02-22 12:16:44 -0800

    Add comment TODO to fix bug with lught enabled set to false and toggling the light source

--- 2DGame/constants.py
+++ 2DGame/constants.py
@@ -2,3 +2,4 @@ W, H = 1200, 800
 P_WIDTH, P_HEIGHT = 150.0, 20.0
 LIGHT_RADIUS = 200  # Example radius for light
 light_enabled = True # Set to True when testing / playing, otherwise False for development
+# TODO Small bug with light_enabled initialized to false and toggling the light source
