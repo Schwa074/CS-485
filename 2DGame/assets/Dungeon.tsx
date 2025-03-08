f0ac3291d03f519d216e6f9e722b317e3017b4b4
Steve Schwarzmann <schwa074@csusm.edu>
2025-03-07 21:01:14 -0800

    Add key and trap closer to enterance for easier animation debugging

--- 2DGame/assets/Dungeon.tsx
+++ 2DGame/assets/Dungeon.tsx
@@ -1,4 +1,13 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="Dungeon" tilewidth="32" tileheight="32" tilecount="768" columns="32">
  <image source="FD_Dungeon_Free.png" width="1024" height="768"/>
+ <tile id="691">
+  <animation>
+   <frame tileid="691" duration="150"/>
+   <frame tileid="690" duration="150"/>
+   <frame tileid="689" duration="150"/>
+   <frame tileid="688" duration="150"/>
+   <frame tileid="689" duration="150"/>
+  </animation>
+ </tile>
 </tileset>
